fbbde9b15448925d7cfb72ca80eb8e7252435bc2
mobinkazemi <s.mobinkazemi@gmail.com>
2024-08-31 21:13:06 +0330

    client id added

--- src/configs/axios.config.ts
+++ src/configs/axios.config.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
+import { v4 } from "uuid";
 
 const apiClient = axios.create({
   baseURL: "http://localhost:8000",
   headers: {
     "Content-Type": "application/json",
+    clientId: v4(),
   },
 });
 
