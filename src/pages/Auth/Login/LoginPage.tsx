dbe5fbdec5b9076fce17020120f212723911c70e
mobinkazemi <s.mobinkazemi@gmail.com>
2024-10-21 22:59:54 +0330

    login - feat - after success login, redirects to home page

--- src/pages/Auth/Login/LoginPage.tsx
+++ src/pages/Auth/Login/LoginPage.tsx
@@ -4,6 +4,8 @@ import { Button, Checkbox, Flex, Form, Input, message } from "antd";
 import TopNavigation from "../../../components/TopNavigation";
 import { loginUser } from "./functions/login-user.function";
 import { TOKEN_KEY_ENUM } from "../../../shared/enums/token.enum";
+import { useNavigate } from "react-router-dom";
+import { ROUTES_ENUM } from "../../../shared/enums/routes.enum";
 
 type FieldType = {
   username?: string;
@@ -11,67 +13,77 @@ type FieldType = {
   remember?: string;
 };
 
-const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
-  const response = await loginUser(values);
+const LoginPage: React.FC = () => {
+  const navigator = useNavigate();
 
-  if (response.result) {
-    localStorage.setItem(TOKEN_KEY_ENUM.ACCESS, response.token as string);
-    message.success(response.message);
-  } else {
-    message.error(response.message);
-  }
+  const onFinish: FormProps<FieldType>["onFinish"] = async (values: any) => {
+    const response = await loginUser(values);
+
+    if (response.result) {
+      localStorage.setItem(TOKEN_KEY_ENUM.ACCESS, response.token as string);
+      message.success(response.message);
+      setTimeout(() => {
+        navigator(ROUTES_ENUM.HOME);
+        window.location.reload();
+      }, 1000);
+    } else {
+      message.error(response.message);
+    }
+  };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <>
+      <TopNavigation></TopNavigation>
+      <Flex justify="center">
+        <Form
+          name="basic"
+          labelCol={{}}
+          wrapperCol={{}}
+          style={{ maxWidth: 500, width: "100%" }}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
+        >
+          <Form.Item<FieldType>
+            label="Username"
+            name="username"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item<FieldType>
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{ offset: 8, span: 16 }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Flex>
+    </>
+  );
 };
-
-const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
-  console.log("Failed:", errorInfo);
-};
-
-const LoginPage: React.FC = () => (
-  <>
-    <TopNavigation></TopNavigation>
-    <Flex justify="center">
-      <Form
-        name="basic"
-        labelCol={{}}
-        wrapperCol={{}}
-        style={{ maxWidth: 500, width: "100%" }}
-        initialValues={{ remember: true }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
-      >
-        <Form.Item<FieldType>
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-        >
-          <Input />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item<FieldType>
-          name="remember"
-          valuePropName="checked"
-          wrapperCol={{ offset: 8, span: 16 }}
-        >
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button type="primary" htmlType="submit">
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
-    </Flex>
-  </>
-);
 
 export default LoginPage;
--- src/pages/Home/functions/viewFunctions/logged_in.tsx
+++ src/pages/Home/functions/viewFunctions/logged_in.tsx
@@ -1,21 +1,23 @@
-import { Button, Flex } from "antd";
-import { useNavigate } from "react-router-dom";
-import { ROUTES_ENUM } from "../../../../shared/enums/routes.enum";
+import { Flex } from "antd";
+import TopNavigation from "../../../../components/TopNavigation";
 
 export const LoggedInContent = () => {
   return (
-    <Flex justify="center" align="center" vertical>
-      <div
-        style={{ marginTop: "5rem", marginBottom: "2rem", direction: "rtl" }}
-      >
-        <div>
-          <h1>شما در سامانه وارد شده‌اید...</h1>
-        </div>
-        <div>
-          {" "}
-          <h1>برای استفاده از امکانات سامانه از منوی بالا استفاده نمایید </h1>
+    <>
+      <TopNavigation></TopNavigation>
+      <Flex justify="center" align="center" vertical>
+        <div
+          style={{ marginTop: "5rem", marginBottom: "2rem", direction: "rtl" }}
+        >
+          <div>
+            <h1>شما در سامانه وارد شده‌اید...</h1>
+          </div>
+          <div>
+            {" "}
+            <h1>برای استفاده از امکانات سامانه از منوی بالا استفاده نمایید </h1>
+          </div>{" "}
         </div>{" "}
-      </div>{" "}
-    </Flex>
+      </Flex>
+    </>
   );
 };
