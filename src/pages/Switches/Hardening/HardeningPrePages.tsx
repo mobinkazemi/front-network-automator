9d9cb86785a6937fae33788934554a59c9784bd1
MohammadrezaMaxsusi <mohammadrezamaxsusi@gmail.com>
2024-11-13 17:34:16 +0330

    checkHardening

--- src/pages/Switches/Hardening/HardeningPrePages.tsx
+++ src/pages/Switches/Hardening/HardeningPrePages.tsx
@@ -1,10 +1,22 @@
-import { Button, Flex } from "antd";
+import { Button, Flex, message } from "antd";
 import { useNavigate, useParams } from "react-router-dom";
 import { ROUTES_ENUM } from "../../../shared/enums/routes.enum";
+import { useEffect, useState } from "react";
+import apiClient from "../../../configs/axios.config";
+import { BACKEND_ROUTES } from "../../../shared/enums/backend.routes.enum";
+import { AxiosResponse } from "axios";
 
 const HardeningPrePage = () => {
   const { switchId } = useParams();
   const navigator = useNavigate();
+  const [testAgain, useTestAgain] = useState<boolean>(false)
+  useEffect(() => {
+    // apiClient.get(BACKEND_ROUTES.SWITCHES_HARDENING.replace(":switchId", switchId as string)).then((data:AxiosResponse<>))
+  },[testAgain])
+  const checkAgainHardening = () => {
+    apiClient.get(BACKEND_ROUTES.SWITCHES_HARDENING.replace(':switchId', switchId as string))
+              .then((data:AxiosResponse<any>) => message.success('تست ها با موفقیت اجرا شد')).catch((e:any) => message.error('اجرای تست ها با خطا مواجه شد'))
+  }
   return (
     <>
       <Flex vertical align="center" style={{ width: "100%" }}>
@@ -70,6 +82,20 @@ const HardeningPrePage = () => {
           }
         >
           گزارش گیری{" "}
+        </Button>
+        <Button
+          type="primary"
+          style={{
+            fontSize: "1.1rem",
+            fontWeight: "bold",
+            letterSpacing: "1px",
+            marginTop: "20px",
+            width: "20%",
+            height: "50px",
+          }}
+          onClick={checkAgainHardening}
+        >
+           تست مجدد
         </Button>
       </Flex>
     </>
