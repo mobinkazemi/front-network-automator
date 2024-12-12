82850489764ef01a0502bf8eb232a64b1b5a0552
mobinkazemi <s.mobinkazemi@gmail.com>
2024-12-12 15:15:02 +0330

    navbar - feat - hardening management added to navbar

--- src/components/TopNavigation.tsx
+++ src/components/TopNavigation.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Icon, { CodepenCircleFilled, HomeOutlined } from "@ant-design/icons";
+import Icon, { CodepenCircleFilled } from "@ant-design/icons";
 import type { MenuProps } from "antd";
 import { Menu } from "antd";
 import { useNavigate } from "react-router-dom";
@@ -19,22 +19,65 @@ const items: MenuItem[] = [
   {
     label: "صفحه اصلی",
     key: ROUTES_ENUM.HOME,
-    icon: <Icon  component={() => (<img  style={{width: '60px'}} src="/douranLogo.png" />)} />,
+    icon: (
+      <Icon
+        component={() => (
+          <img style={{ width: "60px" }} src="/douranLogo.png" />
+        )}
+      />
+    ),
   },
   {
-    label: "سوییچ ها",
+    label: "تجهیزات",
     key: ROUTES_ENUM.__SWITCHES__,
     icon: <CodepenCircleFilled />,
     children: [
-      { label: "لیست سوییچ ها", key: ROUTES_ENUM.SWITCHES_LIST },
-      { label: "ایجاد سوییچ جدید", key: ROUTES_ENUM.SWITCHES_CREATE },
+      {
+        type: "group",
+        label: "سوییچ ها",
+        children: [
+          { label: "لیست", key: ROUTES_ENUM.SWITCHES_LIST },
+          { label: "ایجاد", key: ROUTES_ENUM.SWITCHES_CREATE },
+        ],
+      },
+      {
+        label: "روتر ها",
+        key: NavKeys.ROUTERS,
+        icon: <CodepenCircleFilled />,
+        disabled: true,
+      },
     ],
   },
   {
-    label: "روتر ها",
-    key: NavKeys.ROUTERS,
+    label: "مدیریت هاردنینگ",
+    key: ROUTES_ENUM.__CIS__,
     icon: <CodepenCircleFilled />,
-    disabled: true,
+    children: [
+      {
+        type: "group",
+        label: "سی‌ آی‌ اس",
+        children: [
+          { label: "لیست", key: ROUTES_ENUM.CIS_LIST },
+          { label: "ایجاد", key: ROUTES_ENUM.CIS_CREATE },
+        ],
+      },
+      {
+        type: "group",
+        label: "دسته بندی",
+        children: [
+          { label: "لیست", key: ROUTES_ENUM.CATEGORY_LIST },
+          { label: "ایجاد", key: ROUTES_ENUM.CATEGORY_CREATE },
+        ],
+      },
+      {
+        type: "group",
+        label: "هاردنینگ",
+        children: [
+          { label: "لیست", key: ROUTES_ENUM.HARDENING_LIST },
+          { label: "ایجاد", key: ROUTES_ENUM.HARDENING_CREATE },
+        ],
+      },
+    ],
   },
 ];
 
@@ -74,3 +117,68 @@ const TopNavigation: React.FC = () => {
 };
 
 export default TopNavigation;
+
+// const items: MenuItem[] = [
+//   {
+//     label: "Navigation One",
+//     key: "mail",
+//     icon: <MailOutlined />,
+//   },
+//   {
+//     label: "Navigation Two",
+//     key: "app",
+//     icon: <AppstoreOutlined />,
+//     disabled: true,
+//   },
+//   {
+//     label: "Navigation Three - Submenu",
+//     key: "SubMenu",
+//     icon: <SettingOutlined />,
+//     children: [
+//       {
+//         type: "group",
+//         label: "Item 1",
+//         children: [
+//           { label: "Option 1", key: "setting:1" },
+//           { label: "Option 2", key: "setting:2" },
+//         ],
+//       },
+//       {
+//         type: "group",
+//         label: "Item 2",
+//         children: [
+//           { label: "Option 3", key: "setting:3" },
+//           { label: "Option 4", key: "setting:4" },
+//         ],
+//       },
+//     ],
+//   },
+//   {
+//     key: "alipay",
+//     label: (
+//       <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+//         Navigation Four - Link
+//       </a>
+//     ),
+//   },
+// ];
+
+// const App: React.FC = () => {
+//   const [current, setCurrent] = useState("mail");
+
+//   const onClick: MenuProps["onClick"] = (e) => {
+//     console.log("click ", e);
+//     setCurrent(e.key);
+//   };
+
+//   return (
+//     <Menu
+//       onClick={onClick}
+//       selectedKeys={[current]}
+//       mode="horizontal"
+//       items={items}
+//     />
+//   );
+// };
+
+// export default App;
--- src/shared/enums/routes.enum.ts
+++ src/shared/enums/routes.enum.ts
@@ -13,6 +13,31 @@ export enum ROUTES_ENUM {
   //
   //
   // ####################################
+  //            Hardening & CIS
+  __CIS__ = "/cis",
+  CIS_LIST = "/cis/list",
+  CIS_CREATE = "/cis/create",
+  CIS_UPDATE = "/cis/update",
+  CIS_INFO = "/cis/info",
+  CIS_DELETE = "/cis/delete",
+
+  __CATEGORY__ = "/category",
+  CATEGORY_LIST = "/category/list",
+  CATEGORY_CREATE = "/category/create",
+  CATEGORY_UPDATE = "/category/update",
+  CATEGORY_INFO = "/category/info",
+  CATEGORY_DELETE = "/category/delete",
+
+  __HARDENING__ = "/hardening",
+  HARDENING_LIST = "/hardening/list",
+  HARDENING_CREATE = "/hardening/create",
+  HARDENING_UPDATE = "/hardening/update",
+  HARDENING_INFO = "/hardening/info",
+  HARDENING_DELETE = "/hardening/delete",
+  //
+  //
+  //
+  // ####################################
   //            SWITCHES
   __SWITCHES__ = "/switches",
   SWITCHES_LIST = "/switches/list",
@@ -20,7 +45,7 @@ export enum ROUTES_ENUM {
   SWITCHES_UPDATE = "/switches/update/:switchId",
   SWITCHES_TERMINAL = "/switches/terminal/:switchId",
   SWITCHES_CHECK_CONNECTION_STATUS = "/switches/checkConnectionStatus",
-  //            Hardening
+  //            SWITCHES -> HARDENING
   SWITCHES_PREPAGES_HARDENING = "/switches/hardening/prePages/:switchId",
   SWITCHES_DETAIL_HARDENING = "/switches/hardening/details/:switchId",
   SWITCHES_CHARTS_HARDENING = "/switches/hardening/charts/:switchId",
