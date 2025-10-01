289a4ba51cb8837c9421eb8233d7768b32b5642e
Administrator <gitlab_admin_3f297b@example.com>
2025-10-01 15:04:23 +0000

    Update file vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // server: {
-  //   port: 5173,
-  //   host: true,
-  //   allowedHosts: true,
-  // },
-  // preview: { port: 5173, host: true },
+  server: {
+    port: 5173,
+    host: true,
+    allowedHosts: true,
+  },
+  preview: { port: 5173, host: true },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
