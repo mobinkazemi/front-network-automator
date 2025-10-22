ebcdbce28e0c252c76e47cb1cba1c678c6f3e126
hojat nasiri <hnasiri1997@gmail.com>
2025-10-22 09:48:33 +0330

    feat:error boundary added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Toaster } from "sonner";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from "@tanstack/react-query";
 
 import { Router } from "@/routes/sections";
+import { ErrorBoundary } from "react-error-boundary";
 
 // ----------------------------------------------------------------------
 
@@ -9,11 +14,46 @@ const queryClient = new QueryClient();
 
 // ----------------------------------------------------------------------
 
+function Error({ resetErrorBoundary }) {
+  return (
+    <main class="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
+      <div class="text-center">
+        <h1 class="mt-4 text-5xl font-semibold tracking-tight text-balance text-gray-900 sm:text-7xl">
+          مشکلی پیش آمده است
+        </h1>
+
+        <div class="mt-10 flex items-center justify-center gap-x-6">
+          <a
+            href="/dashboard"
+            class="rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-orange-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+          >
+            بازگشت به پیشخوان
+          </a>
+
+          <button
+            class="text-sm font-semibold text-gray-900"
+            onClick={resetErrorBoundary}
+          >
+            تلاش مجدد
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
+
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Toaster richColors toastOptions={{ className: 'font-sans' }} />
-      <Router />
+      <QueryErrorResetBoundary>
+        {({ reset }) => (
+          <ErrorBoundary onReset={reset} FallbackComponent={Error}>
+            <Toaster richColors toastOptions={{ className: "font-sans" }} />
+
+            <Router />
+          </ErrorBoundary>
+        )}
+      </QueryErrorResetBoundary>
     </QueryClientProvider>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -2,25 +2,17 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import mkcert from "vite-plugin-mkcert";
+// import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mkcert()],
+  plugins: [react(), tailwindcss()],
   server: {
-    port: 8000,
+    port: 5173,
     host: true,
     allowedHosts: true,
-    https: true,
-    proxy: {
-      "/": {
-        target: "https://192.168.11.64:8000",
-        changeOrigin: true,
-        secure: false, // برای self-signed
-      },
-    },
   },
-  preview: { port: 8000, host: true, https: true },
+  preview: { port: 5173, host: true, https: true },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
