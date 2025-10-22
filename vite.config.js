1f4b73a43e6219c4833f94ba852398c1f5d592b2
hojat nasiri <hnasiri1997@gmail.com>
2025-10-22 09:53:48 +0330

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,14 +10,6 @@ export default defineConfig({
     port: 5173,
     host: true,
     allowedHosts: true,
-    https: true,
-    proxy: {
-      "/": {
-        target: "https://192.168.11.64:8000",
-        changeOrigin: true,
-        secure: false, // برای self-signed
-      },
-    },
   },
   preview: { port: 5173, host: true, https: true },
   resolve: {
